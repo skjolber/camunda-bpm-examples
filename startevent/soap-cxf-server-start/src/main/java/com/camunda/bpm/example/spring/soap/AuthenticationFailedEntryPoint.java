4a7c67c61e15909c916f3faf97215bd2349cafd4
Thomas Rørvik Skjølberg <thomas.skjolberg@sparebank1.no>
2016-04-26 14:56:30 +0200

    start example

---
+++ startevent/soap-cxf-server-start/src/main/java/com/camunda/bpm/example/spring/soap/AuthenticationFailedEntryPoint.java
@@ -0,0 +1,29 @@
+package com.camunda.bpm.example.spring.soap;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class AuthenticationFailedEntryPoint implements AuthenticationEntryPoint {
+
+	private String realmName;
+	
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+	    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); 
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + realmName + "\"");
+	}
+	
+    public String getRealmName() {
+        return realmName;
+    }
+
+    public void setRealmName(String realmName) {
+        this.realmName = realmName;
+    }	
+}
